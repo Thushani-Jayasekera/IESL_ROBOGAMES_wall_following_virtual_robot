81d1356e11f07ec2219e1a3689f76e0c4885f84c
dhaura <57411348+dhaura@users.noreply.github.com>
2020-03-24 19:20:43 +0530

    new changes for medical report display

--- Patients/includes/class-autoload.inc.php
+++ Patients/includes/class-autoload.inc.php
@@ -2,7 +2,7 @@
   spl_autoload_register('myAutoLoader');
   function myAutoLoader($className){
     $url = $_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI'];
-    if(strpos($url, 'includes') !== false){
+    if(strpos($url, 'includes') !== false || strpos($url, 'medicalReportDisplay') !== false ){
         $path = "../classes/";
     }
     else if(strpos($url, 'medicalReportForm') !== false){
