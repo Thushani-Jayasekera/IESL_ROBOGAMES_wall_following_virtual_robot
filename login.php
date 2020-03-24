d87eee1f58752f2ad0e27785179ac1b214e16a59
Thushani-Jayasekera <62471034+Thushani-Jayasekera@users.noreply.github.com>
2020-03-24 14:47:03 +0530

    adding styles

--- login.php
+++ login.php
@@ -17,13 +17,18 @@ if(Input::exists()){
             if($login){
                 Redirect::to('index.php');
             }else{
-                echo "Sorry,Log in Failed";
+                echo "<script type='text/javascript'>alert('Sorry,Log in Failed');</script>";
             }
         
         
         }else{
             foreach($validation->errors() as $error){
-                echo $error."<br>";
+                if ($error=='User name is required'){
+                echo "<script type='text/javascript'>alert('Username Required');</script>";
+               }
+               if ($error=='Password is required'){
+                echo "<script type='text/javascript'>alert('Password is required');</script>";
+               }
             }
         }
 }
@@ -31,14 +36,15 @@ if(Input::exists()){
 
 
 ?>
+<link href="RegisterLogin.css" rel="stylesheet">
 <div class="field">
     <form action="" method="POST">
         <h1>Log in</h1>
         <hr>
         <input type="text" name='user_uid' id='user_uid' autocomplete="off" placeholder="ID Number">
         <input type="password" name='user_pwd' id='user_pwd' autocomplete="off" placeholder="Password">
-        <label for="user_remember">
-        <input type="checkbox" name='user_remember' id='user_remember' >Remember Me
+        <label for="user_remember" class="main">Remember Me
+        <input type="checkbox" name='user_remember' id='user_remember'class="urm" ><span ></span>
         </label>
         <input type="hidden" name="token" value="<?php echo Token::generate();?>">
         <input type="submit" value="Log in">
