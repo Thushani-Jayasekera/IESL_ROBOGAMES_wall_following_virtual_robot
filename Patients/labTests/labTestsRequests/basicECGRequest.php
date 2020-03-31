542f56d30fbf03b0899efb7c93108a8ff6fb0703
dhaura <57411348+dhaura@users.noreply.github.com>
2020-03-31 21:46:09 +0530

    Update basicECGRequest.php

--- Patients/labTests/labTestsRequests/basicECGRequest.php
+++ Patients/labTests/labTestsRequests/basicECGRequest.php
@@ -1,4 +1,7 @@
-<?php include_once "../../includes/class-autoload.inc.php" ?>
+<?php
+  session_start();
+  include_once "../../includes/class-autoload.inc.php";
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -15,15 +18,15 @@
       $results = $patientViewObject->showPatientInfo($nic);
 
       $patientInfo = $results[0];
-     ?>
+    ?>
 
-     <form action="../includes/basicECGRequest.inc.php" method="post">
-       <b>Date : </b><input type="date" name="dateOfRequest"><br><br>
-       <?php
+    <form action="../includes/basicECGRequest.inc.php" method="post">
+      <b>Date : </b><input type="date" name="dateOfRequest"><br><br>
+      <?php
         echo "<b>Service No : </b>".$patientInfo['force_id']."<br><b>NIC : </b>".$patientInfo['nic']."<br><b>Rank : </b>".$patientInfo['rank']."
         <br><b>Name : </b>".$patientInfo['first_name']." ".$patientInfo['last_name']."
-        <br><b>Unit : </b>".$patientInfo['unit']."<br><br>";
-        ?>
+        <br><b>Unit : </b>".$patientInfo['regiment']."<br><br>";
+      ?>
 
       <b>Age : </b><input type="number" name="age" placeholder="Ex: 30"><br><br>
 
@@ -54,6 +57,25 @@
         $consMOID = "687543545v";  // should get from a session
 
         echo "<b>Name of Consultant/MO : </b>".$consMOName."<br><b> NIC of Consultant/MO : </b>".$consMOID;
+
+        $_SESSION['nic'] = $nic;
+        $_SESSION['dateOfRequest'] = $_POST['dateOfRequest'];
+        $_SESSION['force_id'] = $patientInfo['force_id'];
+        $_SESSION['rank'] = $patientInfo['rank'];
+        $_SESSION['first_name'] = $patientInfo['first_name'];
+        $_SESSION['last_name'] = $patientInfo['last_name'];
+        $_SESSION['unit'] = $patientInfo['regiment'];
+        $_SESSION['age'] = $_POST['age'];
+        $_SESSION['gender'] = $gender;
+        $_SESSION['ward_no'] = $_POST['ward_no'];
+        $_SESSION['ward'] = $_POST['ward'];
+        $_SESSION['conditions'] = $_POST['conditions'];
+        $_SESSION['leads'] = $_POST['leads'];
+        $_SESSION['shortHistory'] = $_POST['shortHistory'];
+        $_SESSION['consMOName'] = $consMOName;
+        $_SESSION['consMOID'] = $consMOID;
+
+
       ?>
 
       <br><br><br> <button type="submit" name="confirm">Confirm</button>
