01ec9b160bda64bc55be0f04323480d5dc5c467f
dhaura <57411348+dhaura@users.noreply.github.com>
2020-04-05 17:36:59 +0530

    new lab test requests

---
+++ Patients/labTests/classes/histopathologyRequest.class.php
@@ -0,0 +1,41 @@
+
+<?php
+  class HistopathologyRequest{
+    private $nic;
+    private $force_id;
+    private $rank;
+    private $first_name;
+    private $last_name;
+    private $unit;
+    private $age;
+    private $gender;
+    private $telNo;
+    private $ward;
+    private $details;
+    private $consMOName;
+    private $designation;
+    private $consMOID;
+
+    public function __construct($nic, $force_id, $rank, $first_name, $last_name, $unit, $age, $gender, $telNo, $ward, $details, $consMOName, $designation, $consMOID){
+      $this->nic = $nic;
+      $this->force_id = $force_id;
+      $this->rank = $rank;
+      $this->first_name = $first_name;
+      $this->last_name = $last_name;
+      $this->unit = $unit;
+      $this->age = $age;
+      $this->gender = $gender;
+      $this->telNo = $telNo;
+      $this->ward = $ward;
+      $this->details = $details;
+      $this->consMOName = $consMOName;
+      $this->designation = $designation;
+      $this->consMOID = $consMOID;
+
+    }
+
+    public function getProperty($property){
+      return $this->$property;
+    }
+  }
+ ?>
