015a40beb9ded4fa4161291809ce451dfc17cec7
dhaura <57411348+dhaura@users.noreply.github.com>
2020-04-11 21:50:36 +0530

    Update basicECGRequest.inc.php

--- Patients/labTests/includes/basicECGRequest.inc.php
+++ Patients/labTests/includes/basicECGRequest.inc.php
@@ -4,19 +4,19 @@
   include_once "../classes/basicECGRequest.class.php";
 
   $nic = $_SESSION['nic'];
-  $dateOfRequest = $_SESSION['dateOfRequest'];
+  $dateOfRequest = $_POST['dateOfRequest'];
   $force_id = $_SESSION['force_id'];
   $rank = $_SESSION['rank'];
   $first_name = $_SESSION['first_name'];
   $last_name = $_SESSION['last_name'];
   $unit = $_SESSION['unit'];
-  $age = $_SESSION['age'];
+  $age = $_POST['age'];
   $gender = $_SESSION['gender'];
-  $ward_no = $_SESSION['ward_no'];
-  $ward = $_SESSION['ward'];
-  $conditions = $_SESSION['conditions'];
-  $leads = $_SESSION['leads'];
-  $shortHistory = $_SESSION['shortHistory'];
+  $ward_no = $_POST['ward_no'];
+  $ward = $_POST['ward'];
+  $conditions = $_POST['conditions'];
+  $leads = $_POST['leads'];
+  $shortHistory = $_POST['shortHistory'];
   $consMOName = $_SESSION['consMOName'];
   $consMOID = $_SESSION['consMOID'];
 
